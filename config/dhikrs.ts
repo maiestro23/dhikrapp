ce3283c250d5dd3e7be53c2b561c614c43daafd4
Maiestro Nome <maiestro.pro@gmail.com>
2025-04-16 19:54:39 +0200

    Reviewed dhikr list

--- config/dhikrs.ts
+++ config/dhikrs.ts
@@ -11,295 +11,50 @@ export const dhikrs: Dhikr[] = [
     {
         "id": "1",
         "category": "general",
-        "transliteration": "Bismillahi rahmani raheem",
-        "arabicText": "بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيمِ",
-        "translation": "In the name of Allah, the Most Gracious, the Most Merciful"
+        "transliteration": "Alhamdulillah",
+        "arabicText": "ٱلْحَمْدُ ٱللّٰه",
+        "translation": "All praise is due to Allah"
     },
     {
         "id": "2",
+        "category": "general",
+        "transliteration": "Subhanallah",
+        "arabicText": "سُـبْحانَ ٱللّٰه",
+        "translation": "Glory be to Allah"
+    },
+    {
+        "id": "3",
+        "category": "general",
+        "transliteration": "Allahu akbar",
+        "arabicText": "‏اَللّٰهُ أَكْبَرُ‎",
+        "translation": "Allah is the Greatest"
+    },
+    {
+        "id": "4",
         "category": "general",
         "transliteration": "La ilaha illa Allah",
         "arabicText": "لَا إِلٰهَ إِلَّا اللّٰه",
         "translation": "There is no God but Allah"
     },
     {
-        "id": "3",
-        "category": "general",
-        "transliteration": "Alhamdulillah",
-        "arabicText": "ٱلْحَمْدُ ٱللّٰه",
-        "translation": "All praise is due to Allah"
-    },
-    {
-        "id": "4",
-        "category": "general",
-        "transliteration": "Subhanallah",
-        "arabicText": "سُـبْحانَ ٱللّٰه",
-        "translation": "Glory be to Allah"
-    },
-    {
         "id": "5",
         "category": "general",
-        "transliteration": "Allahu akbar",
-        "arabicText": "‏اَللّٰهُ أَكْبَرُ‎",
-        "translation": "Allah is the Greatest"
+        "transliteration": "Astaghfirullah",
+        "arabicText": "أَسْتَغْفِرُ اللّٰه",
+        "translation": "I seek forgiveness from Allah"
     },
     {
         "id": "6",
         "category": "general",
-        "transliteration": "Alhamdulillah",
-        "arabicText": "ٱلْحَمْدُ ٱللّٰه",
-        "translation": "All praise is due to Allah"
+        "transliteration": "Subhanallahi wa bihamdih",
+        "arabicText": "سُبْحَانَ اللّٰهِ وَبِحَمْدِهِ",
+        "translation": "Glory be to Allah and all praise is His"
     },
     {
         "id": "7",
         "category": "general",
-        "transliteration": "Subhanallah",
-        "arabicText": "سُـبْحانَ ٱللّٰه",
-        "translation": "Glory be to Allah"
-    },
-    {
-        "id": "8",
-        "category": "general",
-        "transliteration": "Allahu akbar",
-        "arabicText": "‏اَللّٰهُ أَكْبَرُ‎",
-        "translation": "Allah is the Greatest"
-    },
-    {
-        "id": "9",
-        "category": "general",
-        "transliteration": "Alhamdulillah",
-        "arabicText": "ٱلْحَمْدُ ٱللّٰه",
-        "translation": "All praise is due to Allah"
-    },
-    {
-        "id": "10",
-        "category": "general",
-        "transliteration": "Subhanallah",
-        "arabicText": "سُـبْحانَ ٱللّٰه",
-        "translation": "Glory be to Allah"
-    },
-    {
-        "id": "11",
-        "category": "general",
-        "transliteration": "Allahu akbar",
-        "arabicText": "‏اَللّٰهُ أَكْبَرُ‎",
-        "translation": "Allah is the Greatest"
-    },
-    {
-        "id": "12",
-        "category": "general",
-        "transliteration": "Alhamdulillah",
-        "arabicText": "ٱلْحَمْدُ ٱللّٰه",
-        "translation": "All praise is due to Allah"
-    },
-    {
-        "id": "13",
-        "category": "general",
-        "transliteration": "Subhanallah",
-        "arabicText": "سُـبْحانَ ٱللّٰه",
-        "translation": "Glory be to Allah"
-    },
-    {
-        "id": "14",
-        "category": "general",
-        "transliteration": "Allahu akbar",
-        "arabicText": "‏اَللّٰهُ أَكْبَرُ‎",
-        "translation": "Allah is the Greatest"
-    },
-    {
-        "id": "15",
-        "category": "general",
-        "transliteration": "Alhamdulillah",
-        "arabicText": "ٱلْحَمْدُ ٱللّٰه",
-        "translation": "All praise is due to Allah"
-    },
-    {
-        "id": "16",
-        "category": "general",
-        "transliteration": "Subhanallah",
-        "arabicText": "سُـبْحانَ ٱللّٰه",
-        "translation": "Glory be to Allah"
-    },
-    {
-        "id": "17",
-        "category": "general",
-        "transliteration": "Allahu akbar",
-        "arabicText": "‏اَللّٰهُ أَكْبَرُ‎",
-        "translation": "Allah is the Greatest"
-    },
-    {
-        "id": "18",
-        "category": "general",
-        "transliteration": "Alhamdulillah",
-        "arabicText": "ٱلْحَمْدُ ٱللّٰه",
-        "translation": "All praise is due to Allah"
-    },
-    {
-        "id": "19",
-        "category": "general",
-        "transliteration": "Subhanallah",
-        "arabicText": "سُـبْحانَ ٱللّٰه",
-        "translation": "Glory be to Allah"
-    },
-    {
-        "id": "20",
-        "category": "general",
-        "transliteration": "Allahu akbar",
-        "arabicText": "‏اَللّٰهُ أَكْبَرُ‎",
-        "translation": "Allah is the Greatest"
-    },
-    {
-        "id": "21",
-        "category": "general",
-        "transliteration": "Alhamdulillah",
-        "arabicText": "ٱلْحَمْدُ ٱللّٰه",
-        "translation": "All praise is due to Allah"
-    },
-    {
-        "id": "22",
-        "category": "general",
-        "transliteration": "Subhanallah",
-        "arabicText": "سُـبْحانَ ٱللّٰه",
-        "translation": "Glory be to Allah"
-    },
-    {
-        "id": "23",
-        "category": "general",
-        "transliteration": "Allahu akbar",
-        "arabicText": "‏اَللّٰهُ أَكْبَرُ‎",
-        "translation": "Allah is the Greatest"
-    },
-    {
-        "id": "24",
-        "category": "general",
-        "transliteration": "Alhamdulillah",
-        "arabicText": "ٱلْحَمْدُ ٱللّٰه",
-        "translation": "All praise is due to Allah"
-    },
-    {
-        "id": "25",
-        "category": "general",
-        "transliteration": "Subhanallah",
-        "arabicText": "سُـبْحانَ ٱللّٰه",
-        "translation": "Glory be to Allah"
-    },
-    {
-        "id": "26",
-        "category": "general",
-        "transliteration": "Allahu akbar",
-        "arabicText": "‏اَللّٰهُ أَكْبَرُ‎",
-        "translation": "Allah is the Greatest"
-    },
-    {
-        "id": "27",
-        "category": "general",
-        "transliteration": "Alhamdulillah",
-        "arabicText": "ٱلْحَمْدُ ٱللّٰه",
-        "translation": "All praise is due to Allah"
-    },
-    {
-        "id": "28",
-        "category": "general",
-        "transliteration": "Subhanallah",
-        "arabicText": "سُـبْحانَ ٱللّٰه",
-        "translation": "Glory be to Allah"
-    },
-    {
-        "id": "29",
-        "category": "general",
-        "transliteration": "Allahu akbar",
-        "arabicText": "‏اَللّٰهُ أَكْبَرُ‎",
-        "translation": "Allah is the Greatest"
-    },
-    {
-        "id": "30",
-        "category": "general",
-        "transliteration": "Alhamdulillah",
-        "arabicText": "ٱلْحَمْدُ ٱللّٰه",
-        "translation": "All praise is due to Allah"
-    },
-    {
-        "id": "31",
-        "category": "general",
-        "transliteration": "Subhanallah",
-        "arabicText": "سُـبْحانَ ٱللّٰه",
-        "translation": "Glory be to Allah"
-    },
-    {
-        "id": "32",
-        "category": "general",
-        "transliteration": "Allahu akbar",
-        "arabicText": "‏اَللّٰهُ أَكْبَرُ‎",
-        "translation": "Allah is the Greatest"
-    },
-    {
-        "id": "33",
-        "category": "general",
-        "transliteration": "Alhamdulillah",
-        "arabicText": "ٱلْحَمْدُ ٱللّٰه",
-        "translation": "All praise is due to Allah"
-    },
-    {
-        "id": "34",
-        "category": "general",
-        "transliteration": "Subhanallah",
-        "arabicText": "سُـبْحانَ ٱللّٰه",
-        "translation": "Glory be to Allah"
-    },
-    {
-        "id": "35",
-        "category": "general",
-        "transliteration": "Allahu akbar",
-        "arabicText": "‏اَللّٰهُ أَكْبَرُ‎",
-        "translation": "Allah is the Greatest"
-    },
-    {
-        "id": "36",
-        "category": "general",
-        "transliteration": "Alhamdulillah",
-        "arabicText": "ٱلْحَمْدُ ٱللّٰه",
-        "translation": "All praise is due to Allah"
-    },
-    {
-        "id": "37",
-        "category": "general",
-        "transliteration": "Subhanallah",
-        "arabicText": "سُـبْحانَ ٱللّٰه",
-        "translation": "Glory be to Allah"
-    },
-    {
-        "id": "38",
-        "category": "general",
-        "transliteration": "Allahu akbar",
-        "arabicText": "‏اَللّٰهُ أَكْبَرُ‎",
-        "translation": "Allah is the Greatest"
-    },
-    {
-        "id": "39",
-        "category": "general",
-        "transliteration": "Alhamdulillah",
-        "arabicText": "ٱلْحَمْدُ ٱللّٰه",
-        "translation": "All praise is due to Allah"
-    },
-    {
-        "id": "40",
-        "category": "general",
-        "transliteration": "Subhanallah",
-        "arabicText": "سُـبْحانَ ٱللّٰه",
-        "translation": "Glory be to Allah"
-    },
-    {
-        "id": "41",
-        "category": "general",
-        "transliteration": "Allahu akbar",
-        "arabicText": "‏اَللّٰهُ أَكْبَرُ‎",
-        "translation": "Allah is the Greatest"
-    },
-    {
-        "id": "42",
-        "category": "general",
-        "transliteration": "Alhamdulillah",
-        "arabicText": "ٱلْحَمْدُ ٱللّٰه",
-        "translation": "All praise is due to Allah"
+        "transliteration": "Subhanallahil 'adheem",
+        "arabicText": "سُبْحَانَ اللّٰهِ العَظِيم",
+        "translation": "Glory be to Allah, the great"
     },
 ]
